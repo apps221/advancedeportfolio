5acf21947dc42c1f07dff7377dd6085533b758bf
apps221 <maggie.a.hong@gmail.com>
2025-02-07 12:35:03 -0500

    email js

---
+++ index.js
@@ -0,0 +1,12 @@
+// template_dwduubk
+// service_2pfsocm
+// RX7a4vShMLDa1Bp-8
+function contact(event) {
+    event.preventDefault();
+
+const loading = document.querySelector(".modal__overlay--loading");
+const success = document.querySelector(".modal__overlay--success");
+loading.classList += " .modal__overlay--visible"
+setTimeout(() => {
+}, 500);
+}
