0f36f54961c820d93f8b2ddec218f83c3fc757dc
apps221 <maggie.a.hong@gmail.com>
2025-02-08 12:23:49 -0500

    finished emailjs

--- index.js
+++ index.js
@@ -3,10 +3,22 @@
 // RX7a4vShMLDa1Bp-8
 function contact(event) {
     event.preventDefault();
-
-const loading = document.querySelector(".modal__overlay--loading");
+    const loading = document.querySelector(".modal__overlay--loading");
 const success = document.querySelector(".modal__overlay--success");
 loading.classList += " modal__overlay--visible"
-setTimeout(() => {
-}, 500);
+    emailjs
+    .sendForm(
+        "service_2pfsocm",
+        "template_dwduubk",
+        event.target,
+        "RX7a4vShMLDa1Bp-8",
+    ).then(()=> {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on maggie.a.hong@gmail.com"
+        );
+    })
 }
