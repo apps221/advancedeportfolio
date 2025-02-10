56b577417da0f4ba044a29cf58919761cfdfbd38
apps221 <maggie.a.hong@gmail.com>
2025-02-10 13:00:08 -0500

    finished

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // template_dwduubk
 // service_2pfsocm
 // RX7a4vShMLDa1Bp-8
+const scaleFactor = 1 / 20;
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape"); //returns an array of shapes
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1; //if odd multiple by -1 else multiply by 1
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+let contrastToggle = false;
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme";
+    }
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+}
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector(".modal__overlay--loading");
@@ -21,4 +43,13 @@ loading.classList += " modal__overlay--visible"
             "The email service is temporarily unavailable. Please contact me directly on maggie.a.hong@gmail.com"
         );
     })
+}
+let isModalOpen=false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
